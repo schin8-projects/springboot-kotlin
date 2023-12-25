14a0420bf76f0d827970d9cf5dd38c38414fd75a
Steve Chin <schin8@bu.edu>
2023-12-25 12:41:09 -0500

    Add endpoint; closes #2

---
+++ src/main/kotlin/bu/edu/schin8/tnb/springboot/controllers/HelloWorldController.kt
@@ -0,0 +1,16 @@
+package bu.edu.schin8.tnb.springboot.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    // Use base mapping as the mapping
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello, this is a REST endppoint"
+    }
+}
