58ec591192a6ce2e31b64caa402f1e439c39d5b4
Steve Chin <schin8@bu.edu>
2023-12-28 10:52:56 -0500

    Bank should be a Data Class.  Closes #4

--- src/main/kotlin/bu/edu/schin8/tnb/springboot/model/Bank.kt
+++ src/main/kotlin/bu/edu/schin8/tnb/springboot/model/Bank.kt
@@ -2,36 +2,11 @@ package bu.edu.schin8.tnb.springboot.model
 
 // Data Transfer Object
 
-class Bank {
+// Even though accountNumber is "public", the backing field is not public.  Only the Getter/Setter are public
+// A Data class includes equals/hashCode/toString
 
-    private var accountNumber: String
-    private var trust: Double
-    private var transactionFee: Int
-
-    constructor(accountNumber: String, trust: Double, transactionFee: Int) {
-
-        this.accountNumber = accountNumber
-        this.trust = trust
-        this.transactionFee= transactionFee
-    }
-
-    fun getAccoountNumbrer(): String = accountNumber
-
-    fun setAccountNumber(accountNumber: String) {
-        this.accountNumber = accountNumber
-    }
-
-    // Use Code > Override Methods or ^O... here to generate code
-
-    override fun equals(other: Any?): Boolean {
-        return super.equals(other)
-    }
-
-    override fun hashCode(): Int {
-        return super.hashCode()
-    }
-
-    override fun toString(): String {
-        return super.toString()
-    }
-}
+data class Bank (
+    val accountNumber: String,
+    val trust: Double,
+    val transactionFee: Int
+)
